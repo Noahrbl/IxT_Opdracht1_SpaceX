384844f7494d9d611b85c146d7238b3b45871a3f
Noahrbl <noahrebel@hotmail.com>
2020-03-04 13:59:17 +0100

    Versie 1.0

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+
+document.getElementsByClassName("toggleMenu") [0].onclick = function(){
+	
+	var menu = document.getElementById('navigatie');
+	if(menu.className == "grid-item menu active"){
+		menu.className = "grid-item menu inactive";
+	} else{
+		menu.className = "grid-item menu active";
+	}
+	
+};
+
+
+
+
+
